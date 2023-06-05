39b936d5d3703dd6f09758d19509ad884af54f51
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-05 14:24:24 +0200

    Task 7: fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,8 +2,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a = tuple_a[:2] + (0, 0)[:2 - len(tuple_a)]
     tuple_b = tuple_b[:2] + (0, 0)[:2 - len(tuple_b)]
-    tup = []
-    for i in range(0, len(tuple_a)):
-        tup.append(tuple_a[i]+tuple_b[i])
-    result = tuple(tup)
-    return result
+    new_tuple = ()
+    new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return new_tuple
