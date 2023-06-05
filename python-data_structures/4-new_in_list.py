b6ffd5fe386911050b5a759491b00e47269161dd
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-05 11:26:43 +0200

    Task 4: fix return satement

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,5 +4,5 @@ def new_in_list(my_list, idx, element):
     if idx not in range(0, len(new_list)):
         return new_list
     else:
-        my_list[idx] = element
-        return my_list
+        new_list[idx] = element
+        return new_list
