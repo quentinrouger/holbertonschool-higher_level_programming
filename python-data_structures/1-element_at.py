4ab71c5633f943f906771cc8f87558e874a9c659
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-05 10:00:44 +0200

    Task 1 : change the if statement

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx > len(my_list):
-        return None
-    else:
+    if idx in range(0, len(my_list)):
         return my_list[idx]
+    return None
