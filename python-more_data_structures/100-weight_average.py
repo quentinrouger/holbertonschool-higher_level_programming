d519b105918160e0fb8fcc564d56455e9676b99f
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-08 14:24:26 +0200

    Task 13: average weight

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
+    sum_1 = 0
+    sum_2 = 0
     for score, weight in my_list:
         sum_1 += score * weight
         sum_2 += weight
