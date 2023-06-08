55a663a2119ba8e3745269c0631d66ccbcbc8d2e
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-08 13:52:26 +0200

    Task 14: all on the same line

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    squares = list(map(lambda row: list(map(lambda x: x*x, row)), matrix))
-    return squares
+    return list(map(lambda row: list(map(lambda x: x*x, row)), matrix))
