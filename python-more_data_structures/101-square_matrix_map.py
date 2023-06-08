48a6a1fd36a278902274bd71e69b7282762824b6
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-08 13:47:58 +0200

    Task 14: fix bug

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    squares = list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
+    squares = list(map(lambda row: list(map(lambda x: x*x, row)), matrix))
     return squares
