9fad30e048bcfcdcafe569f2c9d2a4ce48f11de1
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-08 14:22:19 +0200

    Task 100: fix if statement

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == "":
+    if len(my_list) == 0:
         return 0
     for score, weight in my_list:
         sum_1 += score * weight
