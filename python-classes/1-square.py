f02896834ec94cabb4e1962f6ced66da30e600c6
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-12 10:08:03 +0200

    Task 1 : private attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by
+Private instance attribute: size
+"""
+
+
+class Square:
+    """class that defines a square"""
+    def __init__(self, size=None):
+        self.__size = size
