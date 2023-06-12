4d2ee33b5c6137c621b37c8028bc8c63772c01ac
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-12 13:27:44 +0200

    Task 5: Printing a square

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by :
+Private instance attribute: size
+"""
+
+
+class Square:
+    """class that defines a square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print("")
