3770720f6e0f6fd394450b812095105323f837dc
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-13 10:22:30 +0200

    First commit: Task 0 and README file

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """function that adds 2 integers.
+    Args:
+    a: integer (or float)
+    b: integer (or float)
+
+    Return: The addition of a and b
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
