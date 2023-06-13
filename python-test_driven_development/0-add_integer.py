2af4ac64bfbc9f057faba44cca271b29d09301f8
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-13 10:38:33 +0200

    Task 0: Fix documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
-    """function that adds 2 integers.
-    Args:
-    a: integer (or float)
-    b: integer (or float)
+"""
+Function that adds 2 integers
+args: a and b must be integers or float
+Return: the addition of 2 integers
+"""
 
-    Return: The addition of a and b
+
+def add_integer(a, b=98):
+    """
+    function that adds 2 integers.
     """
     if type(a) is not int and type(a) is not float or a is None:
         raise TypeError("a must be an integer")
