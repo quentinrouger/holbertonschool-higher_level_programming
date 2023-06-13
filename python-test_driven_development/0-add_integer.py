670a9a90f1ccf8db98a59e454cfcddb7ee8a3aa8
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-13 12:07:28 +0200

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -14,4 +14,7 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float or b is None:
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    a = int(a)
+    b = int(b)
+
+    return a + b
