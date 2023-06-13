d36d330289144e9c213ea708d34964d576cb2351
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-13 10:28:40 +0200

    Task 0: Add tests when integers are None

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,8 +7,8 @@ def add_integer(a, b=98):
 
     Return: The addition of a and b
     """
-    if type(a) is not int and type(a) is not float:
+    if type(a) is not int and type(a) is not float or a is None:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if type(b) is not int and type(b) is not float or b is None:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
