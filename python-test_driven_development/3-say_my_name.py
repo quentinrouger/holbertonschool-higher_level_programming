9550d3ff7ddff59225c8872840b5e9b9be703ed5
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-13 15:01:46 +0200

    Task 2: Say my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function that prints My name is <first name> <last name>
+Args: first_name, last_name
+Return: My name is <first name> <last name>
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    function that prints My name is <first name> <last name>
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print(f'My name is {first_name} {last_name}')
