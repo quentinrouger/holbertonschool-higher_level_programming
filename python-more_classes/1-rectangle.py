b0280c728aa9b8b3d92476d2fb8d6150a4cf5f68
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-14 13:31:33 +0200

    Task 1: fix order width and height in def __init__

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,8 +10,16 @@ class Rectangle:
     """class that defines a rectangle"""
     def __init__(self, width=0, height=0):
         """Initializes a Rectangle instance."""
-        self.__width = width
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
