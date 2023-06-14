bb372c6ccc1e9610904d052df17ad7736d466a50
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-14 11:29:06 +0200

    Task 6: add tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,14 +9,28 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
+    def test_single_element(self):
+        self.assertEqual(max_integer([1]), 1)
+
     def test_list(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def test_list_unordered(self):
         self.assertEqual(max_integer([1, 4, 3, 2]), 4)
 
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -4, -3, -2]), -1)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([1, -4, -3, -2]), 1)
+
+    def test_duplicated_numbers(self):
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+    
     def test_float_list(self):
         self.assertEqual(max_integer([1.2, 4.1, 3.6, 2.2]), 4.1)
+
+    
 
     def test_isnone(self):
         with self.assertRaises(TypeError):
