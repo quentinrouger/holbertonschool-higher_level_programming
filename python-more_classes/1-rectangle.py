3015a4a15d5e6959bc58087aedffecea64018bb9
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-14 13:39:49 +0200

    Task 1: fix bugs

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -22,20 +22,6 @@ class Rectangle:
         self.__width = width
 
     @property
-    def width(self):
-        """Get the width of the rectangle."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Set the width of the rectangle."""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
     def height(self):
         """Get the height of the rectangle."""
         return self.height
@@ -48,3 +34,17 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
