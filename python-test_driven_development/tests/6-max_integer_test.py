af2086d7645efb40793ff078a6efb3142215ed3e
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-14 11:20:04 +0200

    Task 6: add tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,7 +7,7 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_empty_list(self):
-        self.assertIsNone(max_integer([]))
+        self.assertEqual(max_integer([]), None)
 
     def test_list(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
@@ -17,6 +17,14 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_float_list(self):
         self.assertEqual(max_integer([1.2, 4.1, 3.6, 2.2]), 4.1)
+
+    def test_isnone(self):
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_non_numeric_elements(self):
+        with self.assertRaises(TypeError):
+            max_integer([1, 2, 'a', 4, 5])
 
 
 if __name__ == '__main__':
