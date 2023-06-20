ffd029fa144c35e90be81a365f52eaaa6f608a68
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-20 14:35:17 +0200

    Add: Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Class with the public instance def area(self)
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
