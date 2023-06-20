5cd2cc10388e65cb9193b6750d8fac91216236fc
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-20 16:52:18 +0200

    Add: Task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Write a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Class with the public instance def area(self) and \
+    Public instance method: def integer_validator(self, name, value)
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    class Rectangle that inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.__height = height
+        self.__width = width
