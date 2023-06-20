717a6d348a5caf04b6dd9782b91528a6ddfa3108
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-20 17:48:28 +0200

    Add: Task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Write a class BaseGeometry
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class Square that inherits from Rectangle
+    """
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
