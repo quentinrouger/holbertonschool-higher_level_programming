6b77e15337ff731013a8c27fa4900846cecfaf93
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-20 22:33:04 +0200

    Fix: task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
     """
     Function that read a file
     """
-    with open('my_file_0.txt', encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+    with open(filename) as f:
+        read_data = f.read()
+        print(read_data, end="")
