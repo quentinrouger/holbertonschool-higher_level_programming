229e19a2c222a4e96fa6ae972bc53d92671bb43e
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-22 16:58:45 +0200

    Add: Task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines a student by:
+Public instance attributes: first_nam, last_name, age.
+"""
+
+
+class Student:
+    """
+    Class that defines a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a student object.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            my_dict = {}
+            for element in attrs:
+                if element in self.__dict__:
+                    my_dict[element] = self.__dict__[element]
+            return my_dict
