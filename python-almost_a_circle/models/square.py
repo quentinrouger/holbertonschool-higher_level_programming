0494ff7e5a96bd7115fcaf8393d4f5c67f062f1c
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-26 14:10:09 +0200

    Add: Task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -22,3 +22,12 @@ class Square(Rectangle):
         """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.
                                                  width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
