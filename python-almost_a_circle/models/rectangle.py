5a13372412bf66ec11fca3ea15dcc22c4d80936d
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-26 14:45:48 +0200

    Fix: Task 13

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -122,4 +122,9 @@ class Rectangle(Base):
         """
         Returns the dictionary representation of a Rectangle.
         """
-        return self.__dict__
+        return {'id': self.id,
+                'width': self.width,
+                'height': self.height,
+                'x': self.x,
+                'y': self.y
+                }
