fa57021a18cc3d4b8222856ffc11c21f16d7e0de
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-26 15:00:01 +0200

    Add: Task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 class Base.
 """
+import json
 
 
 class Base:
@@ -19,3 +20,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation \
+              of list_dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
