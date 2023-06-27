0756cf45f811da9df28289f868a59b692186ee42
quentinrouger <quentin_rouger@hotmail.fr>
2023-06-27 10:52:56 +0200

    Add: Task 16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -31,3 +31,18 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Write the JSON string representation \
+            of list_objs to a file
+        """
+        filename = cls.__name__ + ".json"
+        if list_objs is not None:
+            json_data = cls.to_json_string([obj.to_dictionary()
+                                            for obj in list_objs])
+        else:
+            json_data = "[]"
+        with open(filename, "w") as file:
+            json.dump(json_data, file)
