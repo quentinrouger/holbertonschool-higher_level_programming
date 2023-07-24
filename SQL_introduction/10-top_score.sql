f56125c52647c9a902255d3ece07cde85438f594
quentinrouger <quentin_rouger@hotmail.fr>
2023-07-24 03:07:40 -0700

    Add: Task 10

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Script that lists all records of the table second_table of the database hbtn_0c_0 in my MySQL server.
+-- Query for task 10.
+SELECT score, name FROM second_table ORDER BY score DESC;
