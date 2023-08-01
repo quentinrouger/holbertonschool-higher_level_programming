948ded80664fe1dd54ad1a286b533402facbae7d
quentinrouger <quentin_rouger@hotmail.fr>
2023-08-01 02:45:27 -0700

    Fix: Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa."""
+"""
+Script that lists all states from the database hbtn_0e_0_usa.
+"""
 
 import MySQLdb
 import sys
 
 
 def get_all_states(mysql_username, mysql_password, database_name):
-    
+    """
+    Retrieves and prints all the states from the 'states' table in the given
+    MySQL database.
+
+    Parameters:
+        mysql_username (str): The MySQL database username.
+        mysql_password (str): The MySQL database password.
+        database_name (str): The name of the MySQL database.
+
+    Returns:
+        None
+
+    """
+
     conn = MySQLdb.connect(
         host="localhost", port=3306,
         user=mysql_username, passwd=mysql_password, db=database_name
