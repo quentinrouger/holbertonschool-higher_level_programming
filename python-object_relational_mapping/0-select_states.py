5cf3ebb3295f49c45bcf07e28ec9ed3decad22b2
quentinrouger <quentin_rouger@hotmail.fr>
2023-08-01 02:53:14 -0700

    Fix: Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,29 +3,13 @@
 Script that lists all states from the database hbtn_0e_0_usa.
 """
 
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
 
-def get_all_states(mysql_username, mysql_password, database_name):
-    """
-    Retrieves and prints all the states from the 'states' table in the given
-    MySQL database.
-
-    Parameters:
-        mysql_username (str): The MySQL database username.
-        mysql_password (str): The MySQL database password.
-        database_name (str): The name of the MySQL database.
-
-    Returns:
-        None
-
-    """
-
-    conn = MySQLdb.connect(
-        host="localhost", port=3306,
-        user=mysql_username, passwd=mysql_password, db=database_name
-    )
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
@@ -33,8 +17,3 @@ def get_all_states(mysql_username, mysql_password, database_name):
         print(row)
     cur.close()
     conn.close()
-
-if __name__ == "__main__":
-    mysql_username, mysql_password, database_name = sys.argv[1],\
-        sys.argv[2], sys.argv[3]
-    get_all_states(mysql_username, mysql_password, database_name)
