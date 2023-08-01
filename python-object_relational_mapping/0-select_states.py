1b93c7d96faf648c5ea989eb65b598658fcc9a42
quentinrouger <quentin_rouger@hotmail.fr>
2023-08-01 02:33:28 -0700

    Fix: Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Script that lists all states from the database hbtn_0e_0_usa."""
 
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+import MySQLdb
+import sys
 
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+conn = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=mysql_username, passwd=mysql_password, db=database_name
+    )
 cur = conn.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
 query_rows = cur.fetchall()
@@ -15,3 +16,8 @@ for row in query_rows:
     print(row)
 cur.close()
 conn.close()
+
+if __name__ == "__main__":
+    mysql_username, mysql_password, database_name = sys.argv[1],\
+        sys.argv[2], sys.argv[3]
+    get_all_states(mysql_username, mysql_password, database_name)
